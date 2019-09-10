3b86aff19e47b558a03cbceb523e49223b05c72d
Maguin-barboza <magno_barboza@hotmail.com>
2019-09-09 22:28:16 -0300

    Começando tudo novamente

--- CronomtroFísica/Form1.cs
+++ CronomtroFísica/Form1.cs
@@ -15,26 +15,17 @@ namespace CronomtroFísica
 {
     public partial class Form1 : Form
     {
-        //private CronometroController control;
         
 
         public Form1()
         {
             InitializeComponent();
-            //control = new CronometroController();
+            
         }
 
         private void Button1_Click(object sender, EventArgs e)
         {
-            for (int i = 0; i <= 120; i++)
-            {
-                lblSegundos.Text = i.ToString();
-                if (lblSegundos.Text == "60")
-                {
-                    lblMinutos.Text = (Convert.ToInt32(lblMinutos.Text) + 1).ToString();
-                    lblSegundos.Text = "0";
-                }
-            }
+
         }
     }
 }
